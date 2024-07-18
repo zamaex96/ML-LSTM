28fa519d2e3af8cbe8a448e39acbea56f19ea393
MS <maz.zeesal@gmail.com>
2024-07-18 17:20:43 +0900

    Update split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -6,10 +6,10 @@ file_path = 'dataset.csv'
 data = pd.read_csv(file_path)
 
 
-X = data.iloc[:, :-1]
-y = data.iloc[:, -1]
+X = data.iloc[:, :-1] #all rows and all columns except the last one
+y = data.iloc[:, -1]  #all rows and only the last column
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) #70% train data, 30% test data
 
 
 train_data = pd.concat([X_train, y_train], axis=1)
