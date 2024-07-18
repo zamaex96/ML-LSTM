08392d1e15ba0484268ac5bc9376e1d770fa8cc3
MS <maz.zeesal@gmail.com>
2024-07-18 17:17:34 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
 import pandas as pd
-
+number_of_features=4 # change the number_of_features according to the data-set
 class CustomDataset(Dataset):
     def __init__(self, csv_file, transform=None):
         self.data = pd.read_csv(csv_file)
@@ -14,8 +14,8 @@ class CustomDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        features = torch.tensor(self.data.iloc[idx, :2].values, dtype=torch.float32)
-        class_name = torch.tensor(self.data.iloc[idx, 2], dtype=torch.long)
+        features = torch.tensor(self.data.iloc[idx, :number_of_features].values, dtype=torch.float32)
+        class_name = torch.tensor(self.data.iloc[idx, number_of_features], dtype=torch.long)
 
         if self.transform:
             features = self.transform(features)
