8b9505f36c063eedee53c7831e9a4d4eb9a08077
MS <maz.zeesal@gmail.com>
2024-10-22 09:14:08 +0900

    Update confusion_matrix.py

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -82,6 +82,6 @@ with torch.no_grad():
         true_labels.extend(labels.cpu().numpy())
         predicted_labels.extend(predicted.cpu().numpy())
 
-class_names = ["c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "c11", "c12", "c13", "c14", "c15", "c16", "c17"]
+class_names = ["c1", "c2", "c3", "c4"]
 
 plot_confusion_matrix(true_labels, predicted_labels, class_names)
